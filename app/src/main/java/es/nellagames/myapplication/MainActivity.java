4634fc1af48d21bacd6d00db672fbc2f4da8274f
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-07-09 18:30:33 +0200

    doc: add migrations, compiler and config to project

--- app/src/main/java/es/nellagames/myapplication/MainActivity.java
+++ app/src/main/java/es/nellagames/myapplication/MainActivity.java
@@ -1,24 +1,36 @@
-package es.nellagames.myapplication;
+package com.example.mathmagic;
 
+import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import es.nellagames.myapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button startButton = findViewById(R.id.button_start);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, ChallengeActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button shopButton = findViewById(R.id.button_shop);
+        shopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, MagicShopActivity.class);
+                startActivity(intent);
+            }
         });
     }
 }
