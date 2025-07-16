7187a5405b3ff02765498228143797abc2fa9022
Ornella Gigante <ornella-gigante@hotmail.com.ar>
2025-07-16 19:01:58 +0200

    feat: add logic of the buttons of the milestone screen in MilestoneActivity.java

--- app/src/main/java/es/nellagames/myapplication/MilestoneActivity.java
+++ app/src/main/java/es/nellagames/myapplication/MilestoneActivity.java
@@ -1,6 +1,9 @@
 package es.nellagames.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,6 +21,28 @@ public class MilestoneActivity extends AppCompatActivity {
         TextView milestoneMessage = findViewById(R.id.milestone_message);
 
         milestoneMessage.setText("Congratulations! You reached " + milestone + " points!");
-        // Puedes cambiar la imagen del avatar según el milestone si lo deseas
+
+        Button magicShopButton = findViewById(R.id.button_magic_shop);
+        magicShopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MilestoneActivity.this, MagicShopActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        Button mainMenuButton = findViewById(R.id.button_main_menu);
+        mainMenuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MilestoneActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        // Opcional: Cambia la imagen o mensaje dependiendo del milestone
+        // if (milestone >= 100) { ... }
     }
 }
